4029a2fbb7025792efdbbe524cba861690d152fe
Neemias Almeida <ndalmeida@bol.com>
2025-08-04 10:04:29 -0300

    chore: improve tests

--- systemtest/urlshortener_service_test.go
+++ systemtest/urlshortener_service_test.go
@@ -5,121 +5,109 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/ndajr/urlshortener-go/core"
 	proto "github.com/ndajr/urlshortener-go/rpcserver/proto/urlshortener/v1"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func TestURLShortenerService(t *testing.T) {
+func TestURLShorteningService(t *testing.T) {
 	ctx := context.Background()
 
-	t.Run("ShortenAndGet", func(t *testing.T) {
-		originalURL := "https://github.com/ndajr/urlshortener-go"
-
-		shortenRes, err := client.ShortenURL(ctx, &proto.ShortenURLRequest{OriginalUrl: originalURL})
-		require.NoError(t, err)
-		require.NotNil(t, shortenRes)
-		require.NotEmpty(t, shortenRes.GetShortCode())
-
-		getRes, err := client.GetOriginalURL(ctx, &proto.GetOriginalURLRequest{ShortCode: shortenRes.GetShortCode()})
-		require.NoError(t, err)
-		require.NotNil(t, getRes)
-		require.Equal(t, originalURL, getRes.GetOriginalUrl())
-	})
-
-	t.Run("ShortenURL", func(t *testing.T) {
-		testCases := []struct {
-			name   string
-			req    *proto.ShortenURLRequest
-			setup  func(t *testing.T)
-			assert func(t *testing.T, res *proto.ShortenURLResponse, err error)
-		}{
-			{
-				name: "success",
-				req:  &proto.ShortenURLRequest{OriginalUrl: "https://example.com/a/valid/path"},
-				assert: func(t *testing.T, res *proto.ShortenURLResponse, err error) {
+	tests := []struct {
+		name   string
+		setup  func(t *testing.T) []core.URL
+		assert func(t *testing.T, urls []core.URL)
+	}{
+		{
+			name: "ShortenURL/success",
+			assert: func(t *testing.T, _ []core.URL) {
+				originalURL := "https://example.com/a/valid/path"
+				res, err := client.ShortenURL(ctx, &proto.ShortenURLRequest{OriginalUrl: originalURL})
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				require.NotEmpty(t, res.GetShortCode())
+			},
+		},
+		{
+			name: "ShortenURL/failure_on_empty_url",
+			assert: func(t *testing.T, _ []core.URL) {
+				_, err := client.ShortenURL(ctx, &proto.ShortenURLRequest{OriginalUrl: ""})
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, st.Code())
+			},
+		},
+		{
+			name: "ShortenURL/failure_on_url_without_scheme",
+			assert: func(t *testing.T, _ []core.URL) {
+				_, err := client.ShortenURL(ctx, &proto.ShortenURLRequest{OriginalUrl: "google.com"})
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, st.Code())
+			},
+		},
+		{
+			name: "ShortenURL/failure_on_url_too_long",
+			assert: func(t *testing.T, _ []core.URL) {
+				originalURL := "https://" + strings.Repeat("a", 2084)
+				_, err := client.ShortenURL(ctx, &proto.ShortenURLRequest{OriginalUrl: originalURL})
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, st.Code())
+			},
+		},
+		{
+			name: "GetOriginalURL/success",
+			setup: func(t *testing.T) []core.URL {
+				originalURL := "https://github.com/ndajr/urlshortener-go"
+				res, err := client.ShortenURL(ctx, &proto.ShortenURLRequest{OriginalUrl: originalURL})
+				require.NoError(t, err)
+				require.NotNil(t, res)
+				return []core.URL{{LongURL: originalURL, ShortCode: res.ShortCode}}
+			},
+			assert: func(t *testing.T, urls []core.URL) {
+				for _, url := range urls {
+					res, err := client.GetOriginalURL(ctx, &proto.GetOriginalURLRequest{ShortCode: url.ShortCode})
 					require.NoError(t, err)
 					require.NotNil(t, res)
-					require.NotEmpty(t, res.GetShortCode())
-				},
-			},
-			{
-				name: "failure on empty URL",
-				req:  &proto.ShortenURLRequest{OriginalUrl: ""},
-				assert: func(t *testing.T, res *proto.ShortenURLResponse, err error) {
-					require.Error(t, err)
-					st, ok := status.FromError(err)
-					require.True(t, ok)
-					require.Equal(t, codes.InvalidArgument, st.Code())
-				},
-			},
-			{
-				name: "failure on URL without scheme",
-				req:  &proto.ShortenURLRequest{OriginalUrl: "google.com"},
-				assert: func(t *testing.T, res *proto.ShortenURLResponse, err error) {
-					require.Error(t, err)
-					st, ok := status.FromError(err)
-					require.True(t, ok)
-					require.Equal(t, codes.InvalidArgument, st.Code())
-				},
-			},
-			{
-				name: "failure on URL too long",
-				req:  &proto.ShortenURLRequest{OriginalUrl: "https://" + strings.Repeat("a", 2084)},
-				assert: func(t *testing.T, res *proto.ShortenURLResponse, err error) {
-					require.Error(t, err)
-					st, ok := status.FromError(err)
-					require.True(t, ok)
-					require.Equal(t, codes.InvalidArgument, st.Code())
-				},
-			},
-		}
-
-		for _, tc := range testCases {
-			t.Run(tc.name, func(t *testing.T) {
-				if tc.setup != nil {
-					tc.setup(t)
+					require.Equal(t, url.LongURL, res.GetOriginalUrl())
 				}
-				res, err := client.ShortenURL(ctx, tc.req)
-				tc.assert(t, res, err)
-			})
-		}
-	})
-
-	t.Run("GetOriginalURL", func(t *testing.T) {
-		testCases := []struct {
-			name   string
-			req    *proto.GetOriginalURLRequest
-			assert func(t *testing.T, res *proto.GetOriginalURLResponse, err error)
-		}{
-			{
-				name: "failure on not found",
-				req:  &proto.GetOriginalURLRequest{ShortCode: "nonexistent-code"},
-				assert: func(t *testing.T, res *proto.GetOriginalURLResponse, err error) {
-					require.Error(t, err)
-					st, ok := status.FromError(err)
-					require.True(t, ok)
-					require.Equal(t, codes.NotFound, st.Code())
-				},
 			},
-			{
-				name: "failure on empty short code",
-				req:  &proto.GetOriginalURLRequest{ShortCode: ""},
-				assert: func(t *testing.T, res *proto.GetOriginalURLResponse, err error) {
-					require.Error(t, err)
-					st, ok := status.FromError(err)
-					require.True(t, ok)
-					require.Equal(t, codes.InvalidArgument, st.Code())
-				},
+		},
+		{
+			name: "GetOriginalURL/failure_on_not_found",
+			assert: func(t *testing.T, urls []core.URL) {
+				_, err := client.GetOriginalURL(ctx, &proto.GetOriginalURLRequest{ShortCode: "nonexistent-code"})
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.NotFound, st.Code())
 			},
-		}
+		},
+		{
+			name: "GetOriginalURL/failure_on_empty_short_code",
+			assert: func(t *testing.T, urls []core.URL) {
+				_, err := client.GetOriginalURL(ctx, &proto.GetOriginalURLRequest{ShortCode: ""})
+				require.Error(t, err)
+				st, ok := status.FromError(err)
+				require.True(t, ok)
+				require.Equal(t, codes.InvalidArgument, st.Code())
+			},
+		},
+	}
 
-		for _, tc := range testCases {
-			t.Run(tc.name, func(t *testing.T) {
-				res, err := client.GetOriginalURL(ctx, tc.req)
-				tc.assert(t, res, err)
-			})
-		}
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var urls []core.URL
+			if tt.setup != nil {
+				urls = tt.setup(t)
+			}
+			tt.assert(t, urls)
+		})
+	}
 }
